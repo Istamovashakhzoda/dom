ee30f41f5de6943a46ea4487b1b9ccc9a74a53ff
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-18 17:13:58 +0300

    Add files via upload

---
+++ intermedia/script.js
@@ -0,0 +1,20 @@
+"use script"
+
+/*let title=document.querySelector(".Heading1");
+let title2=document.querySelector("#Heading2");
+let desc=document.querySelector("p")*/
+
+/*title.textContent = "20";
+
+title2.textContent = "Lorem ipsum dolor sit amet consecteturLorem ipsum dolor sit amet consectetur";
+
+desc.textContent = "string";*/
+
+let title=document.getElementsByClassName("heading1")
+let title2=document.getElementById("heading2")
+let desc=document.getElementsByTagName("p")
+let input=document.getElementById("fname")
+input.value;
+
+console.log(title.textContent);
+console.log(input);
